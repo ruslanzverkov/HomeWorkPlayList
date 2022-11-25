2bcf20a250ae6ed34c0694cd6216dfa6bcd91fc1
ruslanzverkov <createmed@mail.ru>
2022-11-25 21:22:47 +0300

    2.15.2

---
+++ HomeWork2_16/src/main/java/com/example/homework2_16/HomeWork216Application.java
@@ -0,0 +1,13 @@
+package com.example.homework2_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork216Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork216Application.class, args);
+    }
+
+}
---
+++ HomeWork2_16/src/test/java/com/example/homework2_16/HomeWork216ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework2_16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork216ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
